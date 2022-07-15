11e1147d39ee32769c2f8e31bdf8b80d3c97486e
Lex Gallon <agal0016@student.monash.edu>
2022-07-15 12:12:01 +1000

    Minor formatting, remove duped ThreadedVideo

--- low_cost_mode/display.py
+++ low_cost_mode/display.py
@@ -1,17 +1,20 @@
-import cv2
 import json
-import numpy as np
-from typing import Union, Tuple, List, Dict
 import string
 from datetime import datetime as dt
+from functools import lru_cache
 from pathlib import Path
 from queue import Queue
 from threading import Thread
+from typing import Dict, List, Tuple, Union
+
+import cv2
+import numpy as np
 from tqdm import tqdm
-from functools import lru_cache
 
 # Just to deal with @profile decorator from line_profiler. It's a weird system but whatever
-if type(__builtins__) is not dict or 'profile' not in __builtins__: profile=lambda f:f
+if type(__builtins__) is not dict or "profile" not in __builtins__:
+    profile = lambda f: f
+
 
 def config(key, default=None):
     """Helper method to return value of `key` in `config.json`, else returns `default`"""
@@ -22,8 +25,8 @@ def config(key, default=None):
 
 KEYS = {
     "space": 32,
-    "left": 91, # Note this is actually "[", since left/right arrows are bugged in opencv with QT GUI (https://github.com/opencv/opencv/issues/20215)
-    "right": 93, # This is "]"
+    "left": 91,  # Note this is actually "[", since left/right arrows are bugged in opencv with QT GUI (https://github.com/opencv/opencv/issues/20215)
+    "right": 93,  # This is "]"
     "esc": 27,
     "none": 255,
 }
@@ -33,67 +36,24 @@ KEYS.update({l: ord(l) for l in string.ascii_lowercase})
 KEYCODE_TO_NAME = {v: k for k, v in KEYS.items()}
 
 
-class ThreadedVideo:
-    """
-    Use this to perform read operations on a different thread to eliminate blocking
-    times where possible.
-
-    Stolen from https://pyimagesearch.com/2017/02/06/faster-video-file-fps-with-cv2-videocapture-and-opencv/.
-    """
-    def __init__(self, path, queue_size=512):
-        self.stream = cv2.VideoCapture(path)
-        self.stopped = False
-
-        self.Q = Queue(maxsize=queue_size)
-        self.t = None # To keep track of this Thread
-
-    def start(self):
-        # start a thread to read frames from the file video stream
-        self.t = Thread(target=self.update)
-        # t.daemon = True
-        self.t.start()
-        return self
-
-    def get(self, key):
-        return self.stream.get(key)
-
-    def update(self):
-        # keep looping infinitely
-        while True:
-            # if the thread indicator variable is set, stop the
-            # thread
-            if self.stopped:
-                self.stream.release()
-                return
-            # otherwise, ensure the queue has room in it
-            if not self.Q.full():
-                # read the next frame from the file
-                grabbed, frame = self.stream.read()
-                # if the `grabbed` boolean is `False`, then we have
-                # reached the end of the video file
-                if not grabbed:
-                    self.stop()
-                    return
-                # add the frame to the queue
-                self.Q.put(frame)
-
-    def read(self):
-        # return next frame in the queue
-        return True, self.Q.get()
-
-    def more(self):
-        # return True if there are still frames in the queue
-        return self.Q.qsize() > 0
-
-    def release(self):
-        # indicate that the thread should be stopped
-        self.stopped = True
-
 class Window(ABC):
     DEFAULT_FOURCC = "XVID"
     DEFAULT_EXT = ".avi"
 
-    def __init__(self, *, name:str, show:bool=False, write:bool=False, output_dir:str=None, queue_size=100, flush_thresh=50, fourcc:int=None, fps:int=None, frame_size:Tuple[int, int]=None, **kwargs):
+    def __init__(
+        self,
+        *,
+        name: str,
+        show: bool = False,
+        write: bool = False,
+        output_dir: str = None,
+        queue_size=100,
+        flush_thresh=50,
+        fourcc: int = None,
+        fps: int = None,
+        frame_size: Tuple[int, int] = None,
+        **kwargs,
+    ):
         """
         ! You must use keyword arguments with this initialiser !
         """
@@ -104,7 +64,7 @@ class Window(ABC):
         if fourcc is None:
             print(f"No fourcc provided, assuming default '{self.DEFAULT_FOURCC}'")
             fourcc = cv2.VideoWriter_fourcc(*self.DEFAULT_FOURCC)
-        
+
         # Just storing all initial parameters
         self._name = name
         self._show = show
@@ -123,7 +83,14 @@ class Window(ABC):
             output_file = Path(output_dir) / name
             output_file = str(output_file.with_suffix(self.DEFAULT_EXT))
             self._output_file = output_file
-            self._writer = ThreadedVideoWriter(queue_size=queue_size, flush_thresh=flush_thresh, filename=output_file, fourcc=fourcc, fps=fps, frameSize=frame_size)
+            self._writer = ThreadedVideoWriter(
+                queue_size=queue_size,
+                flush_thresh=flush_thresh,
+                filename=output_file,
+                fourcc=fourcc,
+                fps=fps,
+                frameSize=frame_size,
+            )
 
         self._frame = None
 
@@ -138,17 +105,27 @@ class Window(ABC):
         pass
 
 
-
-
-
 class App:
     FONT = cv2.FONT_HERSHEY_SIMPLEX
 
-    def __init__(self, *, video_src: str, out_dir: str, fourcc: int = None, min_area:int=10, dilate:int=3, movement_thresh:int=40, color_thresh:int=30, movement_pad:int=20, color_pad:int=5, downsample:float=1.0):
+    def __init__(
+        self,
+        *,
+        video_src: str,
+        out_dir: str,
+        fourcc: int = None,
+        min_area: int = 10,
+        dilate: int = 3,
+        movement_thresh: int = 40,
+        color_thresh: int = 30,
+        movement_pad: int = 20,
+        color_pad: int = 5,
+        downsample: float = 1.0,
+    ):
         # TODO: Noticing that I need a min_area, dilate, thresh, and padding for
         # both motion detection AND colour masking. Need to think of nice way of
         # doing this
-        self.reset_HSV() # Not actually resetting obviously
+        self.reset_HSV()  # Not actually resetting obviously
 
         # Filepaths
         self.video_src = video_src
@@ -158,7 +135,7 @@ class App:
         if fourcc is None:
             fourcc = cv2.VideoWriter_fourcc(*"XVID")
         self.fourcc = fourcc
-        
+
         # Image processing parameters
         self.min_area = min_area
         self.movement_thresh = movement_thresh
@@ -172,7 +149,7 @@ class App:
         self.key_delay = 1
 
         # Register the variables we want to save to a json file later. This makes sure
-        # we don't forget how we got a particular output.        
+        # we don't forget how we got a particular output.
         self.register_metadata(
             "video_src",
             "min_area",
@@ -182,13 +159,12 @@ class App:
             "color_pad",
             "dilate",
             "downsample",
-            "fourcc"
+            "fourcc",
         )
-
 
         # Set up Windows?
         self.input_win = Window(name="Input", show=show)
-        
+
     def reset_HSV(self, *args):
         """
         Resets lower and upper bounds for HSV values such that colour thresholding
@@ -203,12 +179,12 @@ class App:
 
     def get_video_cap(self):
         return cv2.VideoCapture(filename=self.video_src)
-        
+
     def show_color_picker():
         """
         TODO: This should pop up a still from a video to let us pick the right
         pixels (and therefore HSV bounds) to select only flowers from a video.
-        
+
         I think I want this such that if user manually passes in HSV values into
         __init__(), then we just use those, else we try to call this method (or
         just ignore it if we're not worried about color).
@@ -218,7 +194,7 @@ class App:
         pass
 
     @profile
-    def start(self, show=True, write=False):    
+    def start(self, show=True, write=False):
         try:
             self.start_time = dt.now()
 
@@ -237,19 +213,28 @@ class App:
                 width, height = self.shape
 
                 # opencv only accepts string filepaths, not Path objects
-                input_write_file = str(out_sub_dir / "input.avi") # for copy of input file, testing opencv encoding
-                diff_write_file = str(out_sub_dir / "diff.avi") # for difference frames
-                trail_write_file = str(out_sub_dir / "trail.avi") # for bee trail video
-                
+                input_write_file = str(
+                    out_sub_dir / "input.avi"
+                )  # for copy of input file, testing opencv encoding
+                diff_write_file = str(out_sub_dir / "diff.avi")  # for difference frames
+                trail_write_file = str(out_sub_dir / "trail.avi")  # for bee trail video
+
                 # Create writers for whichever videos you want to output
                 # input_video_writer = cv2.VideoWriter(filename=input_write_file, fourcc=self.fourcc, fps=fps, frameSize=(width, height))
                 # diff_video_writer = cv2.VideoWriter(filename=diff_write_file, fourcc=self.fourcc, fps=fps, frameSize=(width, height))
                 # trail_video_writer = cv2.VideoWriter(filename=trail_write_file, fourcc=self.fourcc, fps=fps, frameSize=(width, height))
 
                 # input_video_writer = ThreadedVideoWriter(filename=input_write_file, fourcc=fourcc, fps=fps, frameSize=(width, height)).start()
-                diff_video_writer = ThreadedVideoWriter(queue_size=256, flush_thresh=100, filename=diff_write_file, fourcc=self.fourcc, fps=fps, frameSize=(width, height))
+                diff_video_writer = ThreadedVideoWriter(
+                    queue_size=256,
+                    flush_thresh=100,
+                    filename=diff_write_file,
+                    fourcc=self.fourcc,
+                    fps=fps,
+                    frameSize=(width, height),
+                )
                 # trail_video_writer = ThreadedVideoWriter(filename=trail_write_file, fourcc=fourcc, fps=fps, frameSize=(width, height)).start()
-            
+
             # Define windows here for clarity
             if show:
                 cv2.namedWindow("Input", cv2.WINDOW_NORMAL)
@@ -286,7 +271,9 @@ class App:
                 # Resize frame if required
                 if self.downsample < 1:
                     frame = orig_frame.copy()
-                    frame = cv2.resize(frame, dsize=None, fx=self.downsample, fy=self.downsample)
+                    frame = cv2.resize(
+                        frame, dsize=None, fx=self.downsample, fy=self.downsample
+                    )
                 else:
                     frame = orig_frame
 
@@ -295,21 +282,29 @@ class App:
 
                 # Get mask based on movement
                 diff = cv2.absdiff(frame, prev_frame)
-                diff_mask = self.get_mask(diff, thresh=self.movement_thresh, pad=self.movement_pad)
+                diff_mask = self.get_mask(
+                    diff, thresh=self.movement_thresh, pad=self.movement_pad
+                )
 
                 # Get mask based on color
                 hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
                 hsv_low = (self.low_H, self.low_S, self.low_V)
                 hsv_high = (self.high_H, self.high_S, self.high_V)
                 inrange = cv2.inRange(hsv, hsv_low, hsv_high)
-                color_mask = self.get_mask(inrange, thresh=self.color_thresh, pad=self.color_pad, color=None)
+                color_mask = self.get_mask(
+                    inrange, thresh=self.color_thresh, pad=self.color_pad, color=None
+                )
 
                 # Figure out upsample ratio
                 if self.downsample < 1:
                     upsample_x = int(width / frame.shape[1])
                     upsample_y = int(height / frame.shape[0])
-                    diff_mask = diff_mask.repeat(upsample_x, axis=1).repeat(upsample_y, axis=0)
-                    color_mask = color_mask.repeat(upsample_x, axis=1).repeat(upsample_y, axis=0)
+                    diff_mask = diff_mask.repeat(upsample_x, axis=1).repeat(
+                        upsample_y, axis=0
+                    )
+                    color_mask = color_mask.repeat(upsample_x, axis=1).repeat(
+                        upsample_y, axis=0
+                    )
 
                 # Convert masks to actual frames containing masked pixel data
                 diff_frame = np.zeros(orig_frame.shape, np.uint8)
@@ -327,11 +322,19 @@ class App:
                     if not np.any(trail_updates, where=diff_mask):
                         trail_frame[diff_mask] = orig_frame[diff_mask]
                         trail_updates[diff_mask] = True
-                
+
                 # Copy normal frame so we can add frame number without messing up original frame
                 if show:
                     i = self.current_frame_index
-                    cv2.putText(frame_copy, f"Frame {i}/{total_frames}", (50, 100), self.FONT, 2, (0, 0, 255), 3)
+                    cv2.putText(
+                        frame_copy,
+                        f"Frame {i}/{total_frames}",
+                        (50, 100),
+                        self.FONT,
+                        2,
+                        (0, 0, 255),
+                        3,
+                    )
                     frame_copy
 
                     # Show frames of interest
@@ -391,10 +394,12 @@ class App:
 
         # TODO: Think about if I really want to update these values here, or just
         # put them in __init__() with the rest (which is possible, but feels wrong).
-        meta_d.update({
-            "time_taken": str(self.total_time),
-            "start_datetime": str(self.start_time),
-        })
+        meta_d.update(
+            {
+                "time_taken": str(self.total_time),
+                "start_datetime": str(self.start_time),
+            }
+        )
 
         # Sort alphabetically because it's easier to read
         meta_d = dict(sorted(meta_d.items()))
@@ -409,7 +414,7 @@ class App:
         """
         if self.video_cap is not None:
             return int(self.video_cap.get(prop))
-        return -1        
+        return -1
 
     @property
     def current_frame_index(self) -> int:
@@ -434,10 +439,12 @@ class App:
         return width, height
 
     @profile
-    def get_mask(self, frame:np.ndarray, thresh:int, pad: int, color=cv2.COLOR_BGR2GRAY):
+    def get_mask(
+        self, frame: np.ndarray, thresh: int, pad: int, color=cv2.COLOR_BGR2GRAY
+    ):
         """
         Convert a 'soft mask' (e.g. difference between two frames, or all pixels
-        within some range of HSV values) into a masked copy of frame using 
+        within some range of HSV values) into a masked copy of frame using
         rectangular bounding boxes. Specifically, we do the following:
 
             1) Convert to grayscale
@@ -449,10 +456,10 @@ class App:
         """
         if color is not None:
             gray = cv2.cvtColor(frame, color)
-        else: # sometimes we use an already-gray input
+        else:  # sometimes we use an already-gray input
             gray = frame.copy()
         _, threshed = cv2.threshold(gray, thresh, 255, cv2.THRESH_BINARY)
-        
+
         # Only dilate if some kernel size was given
         if self.dilate is not None:
             dilate_kernel = np.ones((self.dilate, self.dilate))
@@ -460,15 +467,17 @@ class App:
         else:
             dilated = threshed
 
-        contours, hierarchy = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv2.findContours(
+            dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+        )
 
-        mask = np.zeros(frame.shape, bool) # Array of all False
+        mask = np.zeros(frame.shape, bool)  # Array of all False
         for contour in contours:
             if cv2.contourArea(contour) < self.min_area:
                 continue
             # Instead of using the exact contours, draw their rectangular bounding boxes
             x0, y0, w, h = cv2.boundingRect(contour)
-            
+
             x1 = x0 + w
             y1 = y0 + h
 
@@ -484,5 +493,13 @@ class App:
 
 video_src = config("INPUT_FILEPATH")
 out_dir = config("OUTPUT_DIRECTORY")
-app = App(video_src=video_src, out_dir=out_dir, movement_pad=5, movement_thresh=80, dilate=None, min_area=0, downsample=1)
+app = App(
+    video_src=video_src,
+    out_dir=out_dir,
+    movement_pad=5,
+    movement_thresh=80,
+    dilate=None,
+    min_area=0,
+    downsample=1,
+)
 # app.start(write=True, show=False)
