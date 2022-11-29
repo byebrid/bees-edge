59af5b52c2eab61e85b9f1e3c9da1ee0986b7384
Lex Gallon <lexlmgallon@gmail.com>
2022-11-29 11:52:47 +1100

    Add very promising multiprocessing example
    - Think I've actually fixed all the issues I was having with keyboard
      interrupts before.
    - Trick is to tell all the child processes to completely ignore keyboard
     interrupts.
    - Instead, pass a common Event into all child processes to tell them an
      interrupt has occurred.
    - Then, when the main process receives an interrupt, it sets this Event.
    - Then, each child process can simply check at the start of their loops
      for this Event to be true, gracefully exiting if so.
    - Did require adding a third process to "ferry" frames from producer
      queue onto consumer queue. Will probably want to rework this at some
      point though, might not be so bad for producer and consumer to share
      queue here. Will need more thought!

--- src/multiprocessing_test2.py
+++ src/multiprocessing_test2.py
@@ -1,65 +1,112 @@
-from multiprocessing import Queue, Process
+from multiprocessing import Queue, Process, Event
+from queue import Empty
 from random import randint
 import time
+import signal
+
+import cv2
 
 
-def producer(queue: Queue, sleep: float = 0.2, num: int = 10):
+def producer(queue: Queue, filename: str, interrupt: Event):
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+    vc = cv2.VideoCapture(filename=filename)
+
     print("Producer: STARTED!", flush=True)
-    try:
-        for i in range(num):
-            item = randint(0, 100)
-            print(f"Producer: putting item#{i}={item} onto queue", flush=True)
-            queue.put(item)
-            time.sleep(sleep)
-    except KeyboardInterrupt:
-        print(f"Producer: RECEIVED KEYBOARDINTERRUPT. Stopping putting items onto producer queue...")
-    finally:
-        print("Producer: Putting None on end of queue")
-        queue.put(None)
-        print("Producer: FINISHED!", flush=True)
+    i = 0
+    while True:
+        if interrupt.is_set():
+            print("Producer: Interrupted!", flush=True)
+            break
+
+        grabbed, frame = vc.read()
+        if not grabbed or frame is None:
+            break
+
+        print(f"Producer: putting item#{i} onto queue", flush=True)
+        queue.put(frame)
+        i += 1
+    
+    print("Producer: Putting None on end of queue", flush=True)
+    queue.put(None)
+    print("Producer: FINISHED!", flush=True)
+    vc.release()
 
 
-def consumer(queue: Queue, sleep: float=0.3):
+def consumer(queue: Queue, filename: str, interrupt: Event):
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    vw = cv2.VideoWriter(filename=filename, fourcc=fourcc, fps=60, frameSize=(1920, 1080))
+
     print("Consumer: STARTED!", flush=True)
     i = 0
     while True:
+        print(f"Consumer: Waiting for item...", flush=True)
         try:
-            print(f"Consumer: Waiting for item...", flush=True)
-            item = queue.get()
-            print(f"Consumer: Got item#{i}={item}", flush=True)
-            time.sleep(sleep)
-            if item is None:
-                print("Consumer: Hit end of queue")
-                break
-
-            i += 1
-        except KeyboardInterrupt:
-            print(f"Consumer: RECEIVED KEYBOARDINTERRUPT. Continuing to empty from queue...", flush=True)
+            frame = queue.get(timeout=10)
+        except Empty:
+            print(f"Consumer: Waited too long for item! Exiting...", flush=True)
+            break
+        print(f"Consumer: Got item#{i}", flush=True)
+        if frame is None:
+            print("Consumer: Hit end of queue", flush=True)
+            break
+        
+        vw.write(frame)
+        i += 1
+    
     print("Consumer: FINISHED!", flush=True)
+    vw.release()
 
+
+def ferry(producer_queue: Queue, consumer_queue: Queue, interrupt: Event):
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+    i = 0
+    while True:
+        print(f"ferry: Getting frame {i} from producer...", flush=True)
+        frame = producer_queue.get(timeout=5)
+
+        print(f"ferry: Putting frame {i} into consumer...", flush=True)
+        consumer_queue.put(frame)
+
+        if frame is None:
+            print(f"ferry: Found frame {i} was None!", flush=True)
+            break
+
+        i += 1
 
 if __name__ == "__main__":
     producer_queue = Queue(maxsize=512)
     consumer_queue = Queue(maxsize=512)
 
-    p1 = Process(target=producer, args=(producer_queue,))
-    p2 = Process(target=consumer, args=(consumer_queue,))
+    interrupt_event = Event()
+    consumer_done_event = Event()
 
-    p2.start()
-    p1.start()
+    input_filename = "data/scaevola/scaevola_long.mp4"
+    output_filename = "test_out.avi"
+    producer_proc = Process(target=producer, args=(producer_queue, input_filename, interrupt_event), name="ProducerProc")
+    ferry_proc = Process(target=ferry, args=(producer_queue, consumer_queue, interrupt_event))
+    consumer_proc = Process(target=consumer, args=(consumer_queue, output_filename, interrupt_event), name="ConsumerProc")
+
+    consumer_proc.start()
+    producer_proc.start()
+    ferry_proc.start()
 
     while True:
         try:
-            item = producer_queue.get()
-            consumer_queue.put(item)
-
-            if item is None:
+            time.sleep(0.5)
+            if not consumer_proc.is_alive() and not producer_proc.is_alive() and not ferry_proc.is_alive():
+                print("main: All child processes appear to have finished? Breaking out of infinite loop...")
                 break
         except KeyboardInterrupt:
-            print("main: RECEIVED KEYBOARDINTERRUPT. Continuing to move items from producer queue onto consumer queue...")
+            print("main: Received KeyboardInterrupt. Setting interrupt event and breaking out of infinite loop...", flush=True)
+            interrupt_event.set()
+            break
 
-    p1.join()
-    p2.join()
+    producer_proc.join()
+    ferry_proc.join()
+    consumer_proc.join()
 
     print(f"Producer queue: {producer_queue.qsize()} items", flush=True)
     print(f"Consumer queue: {consumer_queue.qsize()} items", flush=True)
